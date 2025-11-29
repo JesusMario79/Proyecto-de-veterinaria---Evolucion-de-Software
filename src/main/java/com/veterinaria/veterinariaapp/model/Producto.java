113d1c193060078e25a832835a4c66ed18c55384
JesusMario79 <n00288710@upn.pe>
2025-11-28 20:26:26 -0500

    Creación de la clase Producto

---
+++ src/main/java/com/veterinaria/veterinariaapp/model/Producto.java
@@ -0,0 +1,63 @@
+package com.veterinaria.veterinariaapp.model;
+
+import java.sql.Timestamp;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String categoria;
+    private Timestamp createdAt;
+
+    // Getters y Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
